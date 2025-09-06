af53140dd0e7e9a655d5f4750330337cfd24ef2e
Kamil665 <pochtung@gmail.com>
2025-09-06 23:22:19 +0300

    Update project11.cs

--- project11.cs
+++ project11.cs
@@ -6,12 +6,11 @@ class Program
     {
         int num1, num2;
 
-        // Считываем две целых числа с одной строки
         string[] inputs = Console.ReadLine().Split(' ');
         num1 = int.Parse(inputs[0]);
         num2 = int.Parse(inputs[1]);
 
-        Console.WriteLine(); // Пустая строка
+        Console.WriteLine(); 
 
         Console.WriteLine(num1);
         Console.WriteLine(num2);
