4ed2533acb76cf43606de99c1fe08e1e94938323
Kamil665 <pochtung@gmail.com>
2025-09-06 23:22:04 +0300

    Update project8.cs

--- project8.cs
+++ project8.cs
@@ -6,14 +6,12 @@ class Program
     {
         int A, B, C, D;
 
-        // Считываем четыре целых числа с одной строки
         string[] inputs = Console.ReadLine().Split(' ');
         A = int.Parse(inputs[0]);
         B = int.Parse(inputs[1]);
         C = int.Parse(inputs[2]);
         D = int.Parse(inputs[3]);
 
-        // Выводим их через пробел
         Console.WriteLine($"{A} {B} {C} {D}");
     }
 }
