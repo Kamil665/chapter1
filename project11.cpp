e6b2206dd4b260911ca843ae6687b556703b1926
Kamil665 <pochtung@gmail.com>
2025-09-07 18:28:19 +0300

    Update project11.cpp

--- project11.cpp
+++ project11.cpp
@@ -4,11 +4,15 @@ using namespace std;
 int main() {
 int num1;
 int num2;
+int num3;
+int num4;
 
-cin >> num1 >> num2;
+cin >> num1 >> num2 >> num3 >> num4;
 
 cout << "\n";
 
 cout << num1 << endl;
 cout << num2 << endl;
+cout << num3 << endl;
+cout << num4 << endl;
 }
