6e0751b92daccfa37f4abcc625d37c0bf0b3b8a8
Kamil665 <pochtung@gmail.com>
2025-09-10 13:37:42 +0300

    Create project13.cpp

---
+++ project13.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+
+    int a, b, x, y;
+
+    cin >> a >> b >> x >> y;
+
+    cout << "\n";
+    cout << "2 кг" << endl;
+    cout << "13 17" << endl;
+    cout << "\n";
+    cout << a << " 1" << endl;
+    cout << "19 " << b << endl;
+    cout << "\n";
+    cout << x << " " << y << endl;
+    cout << "5 " << y << endl;
+
+    return 0;
+}
