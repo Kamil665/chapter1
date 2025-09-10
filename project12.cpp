c0bb53ddea9876b107325f81581744db5a74816c
Kamil665 <pochtung@gmail.com>
2025-09-10 13:28:32 +0300

    Create project12.cpp

---
+++ project12.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+setlocale(LC_ALL, "RU");
+int t, v, x, y;
+cin >> t >> v >> x >> y;
+
+cout << "\n";
+cout << "5 10" << endl;
+cout << "7 см" << endl;
+cout << "\n";
+cout << "100 " << t << endl;
+cout << "1949 " << v << endl;
+cout << "\n";
+cout << "x " << x << endl;
+cout << x << " " << y << endl;
+}
