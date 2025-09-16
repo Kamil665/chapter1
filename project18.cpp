3e13aaf8301b119fefadc43f2293eae259c45cf6
Kamil665 <pochtung@gmail.com>
2025-09-16 14:05:51 +0300

    Create project18.cpp

---
+++ project18.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int k;
+    cout << "k = ";
+    cin >> k;
+    cout << "\n";
+    
+    // а)
+    int s1;
+    s1 = 5;
+    s1 = 57;
+    cout << "а) s = " << s1 << endl;
+    
+    // б)
+    int s2;
+    s2 = 6;
+    s2 = -5.2 * s2;
+    s2 = 0;
+    cout << "б) s = " << s2 << endl;
+    
+    // в)
+    int s3;
+    s3 = -7.5;
+    s3 = 2 * s3;
+    cout << "в) s = " << s3 << endl;
+    
+    // г)
+    int s4;
+    s4 = 45;
+    s4 = -25;
+    s4 = s4 + k;
+    cout << "г) s = " << s4 << endl;
+}
