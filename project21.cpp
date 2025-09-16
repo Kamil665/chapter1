4c8a123ba231654deb7304d3b95cfc76e745f4f7
Kamil665 <pochtung@gmail.com>
2025-09-16 14:38:45 +0300

    Create project21.cpp

---
+++ project21.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // а)
+    float a1 = 5.8;
+    float b1 = -7.9;
+    b1 = a1;
+    a1 = b1;
+    cout << "а) a = " << a1 << endl;
+    cout << "   b = " << b1 << endl;
+    
+    cout << "\n";
+    
+    // б)
+    int a2 = 0;
+    double b2 = -9.99;
+    b2 = a2;
+    a2 = b2;
+    cout << "б) a = " << a2 << endl;
+    cout << "   b = " << b2 << endl;
+    
+    return 0;
+}
