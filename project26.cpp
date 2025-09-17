31e8a088004427832ca31d2038adf5bfd1eef25c
Kamil665 <pochtung@gmail.com>
2025-09-17 13:17:37 +0300

    Create project26.cpp

---
+++ project26.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int radius;
+  cout << "Введите радиус окружности: ";
+  cin >> radius;
+
+  int diametr = radius * 2;
+  cout << "\n";
+  cout << "Диаметр окружности: " << diametr << endl;
+}
