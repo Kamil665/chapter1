848a8e85d5230a21b2050fa5c6c600b9ab7ee471
Kamil665 <pochtung@gmail.com>
2025-09-17 14:23:46 +0300

    Create project31.cpp

---
+++ project31.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double num1, num2;
+    cout << "Введите два любых числа: ";
+    cin >> num1 >> num2;
+
+    double arifm = (num1 + num2) / 2;
+    double geometr = sqrt(num1 * num2);
+
+    cout << "\n";
+    
+    cout << "Среднее арифметическое: " << arifm << endl;
+    cout << "Среднее геометрическое: " << geometr << endl;
+}
