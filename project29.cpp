e3fb63991d69b771e7dc6a87e14caccd41dc0e70
Kamil665 <pochtung@gmail.com>
2025-09-17 14:01:36 +0300

    Create project29.cpp

---
+++ project29.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+const double pi = 3.14;
+
+int main() {
+  int radius;
+  cout << "Введите радиус окружности: ";
+  cin >> radius;
+
+  double length = 2 * pi * radius;
+  double area = pi * pow(radius, 2);
+
+  cout << "\n";
+
+  cout << "Длина окружности: " << length << endl;
+  cout << "Площадь круга: " << area << endl;
+
+  return 0;
+}
