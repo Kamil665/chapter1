7cc0ef3995b7e12b995adfa3bf1461f674223e9e
Kamil665 <pochtung@gmail.com>
2025-09-18 13:31:50 +0300

    Create project34.cpp

---
+++ project34.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a, b;
+    do {
+    cout << "Введите значение a: ";
+    cin >> a;
+    
+    cout << "Введите значение b: ";
+    cin >> b;
+    
+    if (a == 0) {
+        cout << "Ошибка! Значение a не должно быть равной нулю!" << endl;
+    }
+    
+    cout << "\n";
+    } while (a == 0);
+    
+    double x = (-b / a);
+    
+    cout << "x = " << x << endl;
+}
